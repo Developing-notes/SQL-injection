f95e19451fc748f8317e16c366d926078dafa2b2
Developing-notes <101121374+Developing-notes@users.noreply.github.com>
2024-01-05 09:25:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-    let Obj={
-        key:"malicious'ex",
-        key1:"SELECT",
-        Key2:"Normal",
-        key3:"TRIGGER"
-    }
-    let array= ["CREATE","ALTER","DROP","TRUNCATE","RENAME","SELECT","INSERT","UPDATE","DELETE","GRANT","REVOKE","COMMIT","ROLLBACK","SAVEPRINT","WHERE","ORDER BY","GROUP BY","HAVING","JOIN","TRIGGER","=",";","'","UNION ALL","UNION"]
+let Obj = {
+  key: "malicious'ex",
+  key1: "SELECT",
+  key2: "Normal",
+  key3: "TRIGGER"
+};
 
-    let isValidParams=true
-    for (const value of Object.values(Obj)) {
-    if (array.some(word => value.toUpperCase().includes(word))) {
-        console.log(`Found a match: ${value}`);
-        isValidParams=false
-    }
-    }
+let array = ["CREATE", "ALTER", "DROP", "TRUNCATE", "RENAME", "SELECT", "INSERT", "UPDATE", "DELETE", "GRANT", "REVOKE", "COMMIT", "ROLLBACK", "SAVEPRINT", "WHERE", "ORDER BY", "GROUP BY", "HAVING", "JOIN", "TRIGGER", "=", ";", "'", "UNION ALL", "UNION"];
 
-    console.log(isValidParams)
+let isValidParams = true;
+
+for (const key in Obj) {
+  if (Obj.hasOwnProperty(key)) {
+    const value = Obj[key];
+    const words = value.toUpperCase().split(/\s+/);
+console.log(words ,">>wordswordswordswords")
+    if (array.some(word => words.includes(word))) {
+      console.log(`Found a match in ${key}: ${value}`);
+      isValidParams = false;
+    }
+  }
+}
+
+console.log(isValidParams);
 
 
 
