a440c3afaec86158c9a591b179aab0bbc954d30d
Developing-notes <101121374+Developing-notes@users.noreply.github.com>
2024-01-07 10:55:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -42,15 +42,4 @@ else{
     console.log("false")
 }
  
-console.log("Welcome to Programiz!");
-3.includes check
-
-const userInput = "malicious' OR 1=1; DROP TABLE users; --";
-const sqlInjectionPattern = /(\b(union|select|insert|update|delete|from|where|drop|alter|create|database|exec|executenonquery|execute)\b|\-\-|\/\*)/i;
-
-if (sqlInjectionPattern.test(userInput)) {
-  console.log("Potential SQL injection detected!");
-} else {
-  console.log("Input seems safe.");
-}
-
+console.log("Welcome to Programiz!"); 
