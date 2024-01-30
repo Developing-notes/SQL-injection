83a9f785df23975b11ca55834602613e1e7c5152
Developing-notes <101121374+Developing-notes@users.noreply.github.com>
2024-01-30 22:29:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -43,3 +43,23 @@ else{
 }
  
 console.log("Welcome to Programiz!"); 
+let Obj = {
+  key: "malicious'ex",
+  key1: "SELECT",
+  key2: "Normal",
+  key3: "TRIGGER"
+};
+
+let array = ["CREATE", "ALTER", "DROP", "TRUNCATE", "RENAME", "SELECT", "INSERT", "UPDATE", "DELETE", "GRANT", "REVOKE", "COMMIT", "ROLLBACK", "SAVEPRINT", "WHERE", "ORDER BY", "GROUP BY", "HAVING", "JOIN", "TRIGGER", "=", ";", "'", "UNION ALL", "UNION"];
+
+// Convert object values to a single string
+const valuesString = Object.values(Obj).join(' ');
+
+// Create a regular expression pattern to match any of the words in the array
+const pattern = new RegExp(array.join('|'), 'i'); // 'i' flag for case-insensitive matching
+
+// Check if the pattern matches any part of the values string
+const isValidParams = !pattern.test(valuesString);
+
+console.log(isValidParams);
+
